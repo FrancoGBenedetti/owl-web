df9add8929834697ed16584ca5b63baf1ef58092
Birdman <francobenedetti2@gmail.com>
2019-08-23 10:05:07 -0400

    owl web

---
+++ js/owl.js
@@ -0,0 +1,95 @@
+
+
+
+$(document).ready(function(){
+
+   
+
+
+   
+   
+
+
+
+
+    $("#local").click(function(){
+        //var x = $("#cajadeprueba").offset();
+        
+        
+      	$('html, body').animate({
+        scrollTop: $('#local-div').offset().top
+    	}, 2000);
+    });
+    $("#no-local").click(function(){
+        //var x = $("#cajadeprueba").offset();
+        
+        
+        $('html, body').animate({
+        scrollTop: $('#no-local-div').offset().top
+        }, 2000);
+    });
+
+    $("#ltl").click(function(){
+        //var x = $("#cajadeprueba").offset();
+        
+        
+        $('html, body').animate({
+        scrollTop: $('#ltl-div').offset().top
+        }, 2000);
+    });
+
+    $("#cons").click(function(){
+        //var x = $("#cajadeprueba").offset();
+        
+        
+        $('html, body').animate({
+        scrollTop: $('#cons-div').offset().top
+        }, 2000);
+    });
+
+
+    $("#cross").click(function(){
+        //var x = $("#cajadeprueba").offset();
+        
+        
+        $('html, body').animate({
+        scrollTop: $('#cross-div').offset().top
+        }, 2000);
+    });
+
+
+    $("#ware").click(function(){
+        //var x = $("#cajadeprueba").offset();
+        
+        
+        $('html, body').animate({
+        scrollTop: $('#ware-div').offset().top
+        }, 2000);
+    });
+
+
+
+      $("#up").click(function(){
+        //var x = $("#cajadeprueba").offset();
+        
+        
+        $('html, body').animate({
+        scrollTop: $('#services-list').offset().top
+        }, 2000);
+    });
+    
+
+     
+
+
+});
+
+
+
+
+
+
+
+
+
+
